91a46c1765bcf2075003f35de435a4b5ab785773
Henrique <henriqueo.vitorino@gmail.com>
2021-10-25 22:28:34 -0300

    Code repost

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#telefone')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+
+nome.style.width = '100%'
